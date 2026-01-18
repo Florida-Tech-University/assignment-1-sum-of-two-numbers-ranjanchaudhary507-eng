ecb5a67e8a059f48aed40f40f9a49c4b080a1454
ranjanchaudhary507-eng <ranjanchaudhary507@gmail.com>
2026-01-19 00:35:53 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // TODO: Read two integers
-      
-
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
         // TODO: Print their sum  using println
+        System.out.println(sum);
       
         scanner.close();
     }
